801623fb257c90879a1a3f63296da8693a948fa9
Derin Yilmaz <dyilmaz2@uwo.ca>
2023-08-13 00:35:34 -0400

    Updated color scheme

---
+++ frontend/src/app/shared/models/Car.ts
@@ -0,0 +1,12 @@
+export class Car{
+    id!: string;
+    name!: string;
+    price!: number;
+    tags?: string[];
+    favourite!: boolean;
+    mileage!: number;
+    imageUrl!: string;
+    make!: string;
+    model!: string;
+    website!: string;
+}
---
+++ frontend/src/data.ts
@@ -0,0 +1,76 @@
+import {Car} from './app/shared/models/Car';
+
+export const sample_cars = Car[] = [
+    {
+        id: 1,
+        name: "BMW",
+        price: 25000,
+        tags: "sport", "2-door",
+        favourite: false,
+        mileage: 50000,
+        imageUrl: ,
+        make: "BMW",
+        model: "3 series",
+        website: ""
+    },
+    {
+        id: 1;
+        name: ;
+        price: ;
+        tags: ;
+        favourite: ;
+        mileage: ;
+        imageUrl: ;
+        make: ;
+        model: ;
+        website: ;
+    },
+    {
+        id: 1;
+        name: ;
+        price: ;
+        tags: ;
+        favourite: ;
+        mileage: ;
+        imageUrl: ;
+        make: ;
+        model: ;
+        website: ;
+    },
+    {
+        id: 1;
+        name: ;
+        price: ;
+        tags: ;
+        favourite: ;
+        mileage: ;
+        imageUrl: ;
+        make: ;
+        model: ;
+        website: ;
+    },
+    {
+        id: 1;
+        name: ;
+        price: ;
+        tags: ;
+        favourite: ;
+        mileage: ;
+        imageUrl: ;
+        make: ;
+        model: ;
+        website: ;
+    },
+    {
+        id: 1;
+        name: ;
+        price: ;
+        tags: ;
+        favourite: ;
+        mileage: ;
+        imageUrl: ;
+        make: ;
+        model: ;
+        website: ;
+    }
+]
